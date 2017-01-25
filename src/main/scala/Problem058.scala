06965c9178eae33570aca0bdfe495f86ecd8f066
Dan Rippel <drippel@crossview.com>
2017-01-25 06:27:55 -0500

    prod 58 done

--- src/main/scala/Problem058.scala
+++ src/main/scala/Problem058.scala
@@ -20,120 +20,61 @@ object Problem058 {
 
     Console.println("058...")
     
-    Console.println( solve(20001) )
-    
-    
+    solve()
   }
   
-  def solve( sz : Int ) : Int = {
-
-    val grid = buildGrid(sz)
+  def solve() : Int = {
     
-    fillGrid( sz, grid )
-    // print(sz,grid)
-
-    val diags = collectDiags(sz,grid).toSet
-    val parts = diags.partition( isPrime( _ ) )
-   
-    val b1 = BigDecimal( diags.size )
-    val b2 = BigDecimal( parts._1.size )
-    
-    val ratio = b2 / b1 
-    Console.println( sz +" "+ ratio)
-    sz
-    
-    /*
-    if( ratio < 0.1 ){
-      sz
-    }
-    else {
-      solve(sz+2)
-    }
-    * 
-    */
-
-  
-  }
-  
-  def collectDiags( n : Int, g : Array[Array[Int]] ) : List[Int] = {
-    
-    def inner( cx : Int, cy : Int, dx : Int, dy : Int, accum : List[Int] ) : List[Int] = {
+    def inner( n : Int, accum : (Int,Int) ) : Int = {
       
-      if( !isValid( n, cx, cy ) ){
-        accum
+      val cs = calcCorners(n,accum)
+      val ratio = BigDecimal(cs._1) / BigDecimal(cs._2)
+      
+      Console.println( n +" "+ ratio )
+      
+      if( ratio < 0.10 ){
+        n
       }
       else {
-        inner( cx + dx, cy + dy, dx, dy, accum :+ g(cx)(cy) )
+        inner( n + 2, cs )
       }
     }
     
-    inner( 0, 0, 1, 1, List() ) ++ inner( (n -1), 0, -1, 1, List() ) 
+    inner(3, (0,1) )
   }
   
-  class Move( val x : Int, val y : Int )
-  case class Left() extends Move( -1, 0 )
-  case class Down() extends Move( 0, 1 )
-  case class Right() extends Move( 1, 0 )
-  case class Up() extends Move( 0, -1 )
-  
-  def nextDir( m : Move ) : Move = {
-    m match {
-      case Left() => { Down() }
-      case Down() => { Right() }
-      case Right() => { Up() }
-      case Up() => { Left() }
-    }
-  }
-  
-  def fillGrid( n : Int, g : Array[Array[Int]] ) = {
+  def calcCorners( n : Int, accum : (Int,Int) ) : (Int,Int) = {
     
-    // start at the top right - 1001,0
-    var currX = n 
-    var currY = 0
-    var currDir : Move = Left()
-    
-    for( i <- ( n * n ) until 0 by -1 ){
-      // move 
-      var nextX = currX + currDir.x
-      var nextY = currY + currDir.y
-      
-      if( !isValid( n, nextX, nextY ) || !isEmpty( g, nextX, nextY ) ){
-        currDir = nextDir( currDir )
-        currX = currX + currDir.x
-        currY = currY + currDir.y
-      }
-      else {
-        currX = nextX
-        currY = nextY
-      }
-      
-      // fill
-      g(currX)(currY) = i
-    }
-     
-    
-  }
-  
-  def isValid( n : Int, x : Int, y : Int ) : Boolean = {
-    x >= 0 && x < n && y >= 0 && y < n 
-  }
+        val c1 = ( n * n )
+        var naccum = (accum._1,accum._2+1)
+        
+        val c2 = c1 - ( n - 1 )  
+        naccum = if( isPrime2(c2) ){
+          (naccum._1 + 1,naccum._2+1)
+        }
+        else {
+          (naccum._1,naccum._2+1)
+        }
 
-  def isEmpty( g : Array[Array[Int]], x : Int, y : Int ) : Boolean = {
-    g(x)(y) == 0
+        val c3 = c2 - ( n - 1 )  
+        naccum = if( isPrime2(c3) ){
+          (naccum._1 + 1,naccum._2+1)
+        }
+        else {
+          (naccum._1,naccum._2+1)
+        }
+
+        val c4 = c3 - ( n - 1 )  
+        naccum = if( isPrime2(c4) ){
+          (naccum._1 + 1,naccum._2+1)
+        }
+        else {
+          (naccum._1,naccum._2+1)
+        }
+        
+        naccum
+
+
   }
   
-  def print(n : Int, g : Array[Array[Int]] ) = {
-    
-    for( y <- 0 until n ){
-      for( x <- 0 until n ){
-        Console.print(g(x)(y) +"  " )
-      }
-      Console.print( '\n' )
-    }
-    
-  }
-  
-  def buildGrid( n : Int ) : Array[Array[Int]] = {
-    Array.fill[Int](n, n)(0)
-  }
 }
